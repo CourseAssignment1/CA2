45797281eb730a276d63d6bcf0c71efc69a5842b
GertMadsen <cph-gm50@cphbusiness.dk>
2018-03-13 09:59:15 +0100

    facade added og sql script added

---
+++ src/main/java/facade/Facade.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author Gert Lehmann Madsen
+ */
+public class Facade {
+
+    EntityManagerFactory emf;
+
+    public Facade(EntityManagerFactory emf) {
+        this.emf = emf;
+    }  
+    
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+}
