8638684ee348a94ec35b19c7178e1346db011a18
Lene Skov <lsko@itu.dk>
2018-03-13 10:55:48 +0100

    fordeling af metoder i facaden ml Lene og Mikkel

--- src/main/java/facade/Facade.java
+++ src/main/java/facade/Facade.java
@@ -27,59 +27,68 @@ public class Facade {
         return emf.createEntityManager();
     }
     
+    //Lene
     public Person getPerson(long id) {
         Person person = null;
         
         return person;
     }
-
+    
+    //Mikkel
     public Person addPerson(Person person) {
         
         return person;
     }
 
-    
+    //Lene
     public List<Person> getPersons(){
         List<Person> persons = new ArrayList();
         
         return persons;
     }
-
+    
+    //Mikkel
     public List<Person> getPersonsWithHobby(Hobby hobby){
         List<Person> persons = new ArrayList();
         
         return persons;
     }
-
+    
+    //Lene
     public List<Person> getPersonsFromZip(CityInfo cityInfo){
         List<Person> persons = new ArrayList();
         
         return persons;
     }
     
+    //Mikkel
     public Company getCompany(long cvr){
         Company company = null;
         
         return company;
     }
 
+    //Lene
     public Company addCompany(Company company) {
         
         return company;
     }
     
+    //Mikkel
     public int countPersonsWithHobby(Hobby hobby) {
         int count = 0;
         
         return count;
     }
     
+    //Lene
     public List<CityInfo> getZips() {
         List<CityInfo> cityInfos = new ArrayList();
         
         return cityInfos;
     }
     
+    //Mikkel
     public List<Company> getCompaniesWithMoreThanXEmployees(int amount) {
         List<Company> companies = new ArrayList();
         
