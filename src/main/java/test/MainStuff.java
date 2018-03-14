c87452c8a39861911362893661805b15fddccd47
GertMadsen <cph-gm50@cphbusiness.dk>
2018-03-14 12:14:32 +0100

    MainStuff printout of persons

--- src/main/java/test/MainStuff.java
+++ src/main/java/test/MainStuff.java
@@ -5,9 +5,7 @@
  */
 package test;
 
-
-import entities.Company;
-import entities.Person;
+import entities.*;
 import facade.Facade;
 import java.util.List;
 import javax.persistence.EntityManagerFactory;
@@ -20,17 +18,37 @@ import javax.persistence.Persistence;
 public class MainStuff {
 
     public static void main(String[] args) {
-        
+
 //        Persistence.generateSchema("dat3sem_CA2_war_1.0PU2", null);
-        
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("dat3sem_CA2_war_1.0PU2");
         Facade facade = new Facade(emf);
 
+//        Person testPerson = facade.getPerson(9);
+//        System.out.println(testPerson.getFirstName()+" "+testPerson.getLastName());
+//        System.out.println(testPerson.getAddress().getStreet()+ " "+testPerson.getAddress().getAdditionalInfo());
+//        System.out.println(testPerson.getAddress().getCityinfo().getZip() + " " + testPerson.getAddress().getCityinfo().getCity());
+//        System.out.println(testPerson.getEmail());
+//        List<Phone> phoneNumbers = testPerson.getPhoneNumbers();
+//        for (Phone phoneNumber : phoneNumbers) {
+//            System.out.println(phoneNumber.getDescription() + " : " + phoneNumber.getNumber());
+//        }
+
         List<Person> testPersons = facade.getPersons();
         for (Person testPerson : testPersons) {
-        System.out.println(testPerson.getFirstName()+" "+testPerson.getLastName());   
+            System.out.println(testPerson.getFirstName() + " " + testPerson.getLastName());
+            System.out.println(testPerson.getAddress().getStreet() + " " + testPerson.getAddress().getAdditionalInfo());
+            System.out.println(testPerson.getAddress().getCityinfo().getZip() + " " + testPerson.getAddress().getCityinfo().getCity());
+            System.out.println(testPerson.getEmail());
+            List<Phone> phoneNumbers = testPerson.getPhoneNumbers();
+            for (Phone phoneNumber : phoneNumbers) {
+                System.out.println(phoneNumber.getDescription() + " : " + phoneNumber.getNumber());
+            }
+            List<Hobby> hobbies = testPerson.getHobbies();
+            for (Hobby hobby : hobbies) {
+                System.out.println(hobby.getName() + " : " + hobby.getDescription());
+            }
+            System.out.println("");
         }
-        
     }
 
 }
